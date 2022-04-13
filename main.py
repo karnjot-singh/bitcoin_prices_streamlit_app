84c7955b8247738d3d71035be46233fb093aa9dd
Karnjot Singh <karnjot.singh@smu.ca>
2022-04-13 20:12:27 -0300

    crypto coin selection added

--- main.py
+++ main.py
@@ -3,19 +3,24 @@ import streamlit as st
 
 st.title("Bitcoin Prices")
 days_slider = st.slider("No of Days", max_value=365, min_value=1, value=30)
+st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
 currency_radio = st.radio(
      "Currency",
      ('CAD', 'INR', 'USD'))
 
+coin_radio = st.radio(
+     "Crypto Coin",
+     ('BITCOIN', 'DOGECOIN', 'ETHEREUM'))
+
 def plot_line(df, currency):
     if df is None:
-        print("Failed to fetch data")
+        st.error("Failed to fetch data")
     else:
         st.line_chart(df)
         avg = df[currency.upper()].mean()
-        st.write("Average price during this time was {:.2f} {}".format(avg, currency.upper()))
+        st.markdown("Average price during this time was **{:.2f} {}**".format(avg, currency.upper()))
 
 if __name__ == "__main__":
     api = api.API()
-    df = api.fetchData('bitcoin', currency_radio, days_slider)
+    df = api.fetchData(coin_radio.lower(), currency_radio, days_slider)
     plot_line(df, currency_radio)
